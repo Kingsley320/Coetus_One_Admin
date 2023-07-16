d19f7c841ff8aff9b6932ea4556f8d77f58aeaa3
Kingsley320 <75090590+Kingsley320@users.noreply.github.com>
2023-07-16 23:07:52 +0100

    Initial commit

---
+++ src/components/PropertyCards.jsx
@@ -0,0 +1,31 @@
+import { IoEye, IoHeartOutline } from "react-icons/io5";
+import "./css/PropertyPage.css"
+
+function PropertyCards(props) {
+    return (
+        <div className="property-card">
+            {/* <div className="top">
+                <IoEye />
+                <h4>{props.views} views</h4>
+            </div> */}
+            <div className="middle">
+                <img src={props.image} alt="" />
+            </div>
+            <div className="bottom">
+                <div className="price-like">
+                    <h3>{props.name}</h3>
+                    <h3>${props.price}</h3>
+                </div>
+                <p>{props.area} Sq ft</p>
+                <p>{props.street}</p>
+                <p>{props.city}</p>
+                <div className="property-status">
+                    <p>{props.status}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default PropertyCards;
