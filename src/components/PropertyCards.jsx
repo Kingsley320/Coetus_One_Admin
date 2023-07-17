2555ef153867ee1590020301472001c869549803
Kingsley320 <75090590+Kingsley320@users.noreply.github.com>
2023-07-17 22:26:56 +0100

    component showing a property card

--- src/components/PropertyCards.jsx
+++ src/components/PropertyCards.jsx
@@ -8,18 +8,20 @@ function PropertyCards(props) {
                 <IoEye />
                 <h4>{props.views} views</h4>
             </div> */}
-            <div className="middle">
-                <img src={props.image} alt="" />
+            <div className="middle ">
+                <img src={props.image} alt="property" />
             </div>
             <div className="bottom">
                 <div className="price-like">
                     <h3>{props.name}</h3>
                     <h3>${props.price}</h3>
                 </div>
-                <p>{props.area} Sq ft</p>
+                <div className="text-left">
+                <p >{props.area} Sq ft</p>
                 <p>{props.street}</p>
                 <p>{props.city}</p>
-                <div className="property-status">
+                </div>
+                <div className="property-status ">
                     <p>{props.status}</p>
                 </div>
             </div>
