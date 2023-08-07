a0944b58f1f719d1fdc127ce5616de994ed07bc8
Kingsley320 <75090590+Kingsley320@users.noreply.github.com>
2023-08-07 22:55:49 +0100

    Adjusted the appointment card components design

--- src/components/AppointmentCard.jsx
+++ src/components/AppointmentCard.jsx
@@ -6,18 +6,19 @@ export default function AppointmentCard(props) {
                 <div className="mb-1 shadow-md">
                     <img src={props.img} alt={props.alt} className="h-40 w-full object-cover" />
                 </div>
-                <div className="p-3 px-5 w-full leading-8">
+                <div className="p-3 px-5 w-full leading-8 text-left">
                     <div className="w-full   justify-between text-sm">
+                        <p ><span className="font-semibold">Name: </span> {props.city}</p>
                         <p><span className="font-semibold">Price: </span> {props.price}</p>
-                        <p ><span className="font-semibold">City: </span> {props.city}</p>
                     </div>
-                    <div className="text-left">
+                    <div>
                         <p><span className="font-semibold">Prospective: </span>{props.tenant}</p>
                     </div>
-                    <div className="flex justify-between text-left">
-                        <p><span className="font-semibold">Time: </span>{props.time}</p>
+                    <div>
+                        <p><span className="font-semibold">Time: </span>{props.from} - {props.to}</p>
                         <p><span className="font-semibold">Date: </span>{props.date}</p>
                     </div>
+                    <p><span className="font-semibold">Completed: </span>{props.completed}</p>
                 </div>
             </div>
         </>
