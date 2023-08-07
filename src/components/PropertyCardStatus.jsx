dddb3470161c414e09749836d3e635a53e63588f
Kingsley320 <75090590+Kingsley320@users.noreply.github.com>
2023-08-07 22:57:09 +0100

    property verification status redesign

--- src/components/PropertyCardStatus.jsx
+++ src/components/PropertyCardStatus.jsx
@@ -3,23 +3,23 @@ import "./css/PropertyPage.css"
 
 function PropertyCards(props) {
     return (
-        <div className="property-card">
+        <div className="w-80 h-96 rounded-xl overflow-hidden border-2 border-gray-200">
             {/* <div className="top">
                 <IoEye />
                 <h4>{props.views} views</h4>
             </div> */}
-            <div className="middle ">
-                <img src={props.image} alt="property" />
+            <div className="w-full h-48 overflow-hidden">
+                <img src={props.image} alt="property" className="w-full "/>
             </div>
-            <div className="bottom">
-                <div className="price-like">
-                    <h3>{props.name}</h3>
-                    <h3>${props.price}</h3>
+            <div className="text-left">
+                <div className="flex justify-between mr-2">
+                    <h3>Name: {props.name}</h3>
+                    <h3>Price: ${props.price}</h3>
                 </div>
                 <div className="text-left">
-                <p >{props.area} Sq ft</p>
-                <p>{props.street}</p>
-                <p>{props.city}</p>
+                <p >Size: {props.area} </p>
+                <p>Address: {props.street}</p>
+                <p>City: {props.city}</p>
                 </div>
                 <div className="property-status ">
                     <p>{props.status}</p>
